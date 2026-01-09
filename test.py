e7c85d107c70590760af9456e4dc3861dcd037d2
JM <kjm8024@gmail.com>
2026-01-09 14:04:02 +0900

    Counting Array

--- test.py
+++ test.py
@@ -1,20 +1,15 @@
-def binary_search(search_value, array) :
-    lower_point = 0
-    upper_point = len(array) - 1
-    while (lower_point <= upper_point) :
-        mid_point = (lower_point + upper_point) // 2
-        if (search_value == array[mid_point]) :
-            return 1
-        if (search_value < array[mid_point]) :
-            upper_point = mid_point - 1
-        if (search_value > array[mid_point]) :
-            lower_point = mid_point + 1
-    return 0
+arr = [0] * 10
+for i in range(10) :
+    arr[i] = int(input())
+for i in range(len(arr)) :
+    arr[i] %= 42
+test = [0 for i in range(42)]
+for value in arr :
+    test[value] += 1
 
-N = int(input())
-A = list(map(int, input().split()))
-M = int(input())
-B = list(map(int, input().split()))
-A.sort()
-for test_value in B :
-    print(binary_search(test_value, A))
+count = 0
+for i in range(len(test)) :
+    if (test[i] >= 1) :
+        count += 1
+
+print(count)
