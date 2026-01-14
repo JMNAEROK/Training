ebba0c0bfb4ae55c44337a0e17d6d4ec9457c8e5
JM <kjm8024@gmail.com>
2026-01-14 13:30:54 +0900

    Implement intersection with hash table

--- test.py
+++ test.py
@@ -1,23 +1,12 @@
-N = int(input())
-time = list(map(int, input().split()))
-
-for index in range(1, len(time)) :
-    temp_value = time[index]
-    position = index - 1
-    while(position >= 0) :
-        if(time[position] > temp_value) :
-            time[position + 1] = time[position]
-            position -= 1
-        else :
-            break
-    time[position + 1] = temp_value
-print(time)
-
-total_time = 0
-for i in range(len(time)) :
-    sum = 0
-    for j in range(i + 1) :
-        sum += time[j]
-    total_time += sum
-
-print(total_time)
+def intersection (array1, array2) :
+    array3 = list()
+    hash_table = {}
+    for key in array1 :
+        hash_table[key] = True
+    for search_value in array2 :
+        if (search_value in hash_table) :
+            array3.append(search_value)
+    return array3
+arr1 = list(map(int, input().split()))
+arr2 = list(map(int, input().split()))
+print(intersection(arr1, arr2))
