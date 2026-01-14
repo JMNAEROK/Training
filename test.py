29fa613466fcb2b8c5dd831042efcc4597356d88
JM <kjm8024@gmail.com>
2026-01-14 15:24:10 +0900

    Prefix Check with hash table

--- test.py
+++ test.py
@@ -1,12 +1,13 @@
-def find_non_duplication (text) :
+def find_the_same_prefix(phone_num) :
     hash_table = {}
-    for value in text :
-        if (hash_table.get(value)) :
-            hash_table[value] += 1
-        else :
-            hash_table[value] = 1
-    for value in text :
-        if (hash_table[value] == 1) :
-            return value
-text = input()
-print(find_non_duplication (text))
+    for i in range(phone_num) :
+        hash_table[(input())] = True
+    for value in hash_table :
+        for j in range(1, len(value)) :
+            if(hash_table.get(value[:j]) == True) :
+                return "NO"
+    return "YES"
+test_case_num = int(input())
+for i in range(test_case_num) :
+    phone_num = int(input())
+    print(find_the_same_prefix(phone_num))
