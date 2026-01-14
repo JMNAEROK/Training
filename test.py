3d5e59cf61fd16ab8900e73f72506337abce1ed5
JM <kjm8024@gmail.com>
2026-01-14 13:45:09 +0900

    Implement find_duplication with hash table

--- test.py
+++ test.py
@@ -1,12 +1,10 @@
-def intersection (array1, array2) :
-    array3 = list()
+def find_duplication(array) :
     hash_table = {}
-    for key in array1 :
-        hash_table[key] = True
-    for search_value in array2 :
-        if (search_value in hash_table) :
-            array3.append(search_value)
-    return array3
-arr1 = list(map(int, input().split()))
-arr2 = list(map(int, input().split()))
-print(intersection(arr1, arr2))
+    for value in array :
+        if (hash_table.get(value) == True) :
+            return value
+        else :
+            hash_table[value] = True
+
+arr = list(map(str, input().split()))
+print(find_duplication(arr))
