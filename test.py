31d554cc6bcf842ef5d344ebff339b438353a90d
JM <kjm8024@gmail.com>
2026-01-15 17:23:53 +0900

    Postfix Notation with Stack

--- test.py
+++ test.py
@@ -1,38 +1,33 @@
-class Stack :
-    def  __init__ (self) :
-        self.data = []
-    def push (self, element) :
-        self.data.append(element)
-    def pop (self) :
-        if(len(self.data)) > 0 :
-            return self.data.pop()
-        else :
-            return None
-    def read (self) :
-        if (len(self.data) > 0) :
-            return self.data[-1]
-        else :
-            return None
-class Linter :
+import string
+class stack :
     def __init__ (self) :
-        self.stack = Stack()
-    def lint(self, text) :
-        while (self.stack.read()) :
-            self.stack.pop()
-        matching_braces = {"(" : ")", "{" : "}", "[" : "]"}
-        for char in text :
-            if char in matching_braces.keys() :
-                self.stack.push(char)
-            elif char in matching_braces.values() :
-                if not self.stack.read() :
-                    return char + " 여는 괄호가 없음"
-                else :
-                    popped_opening_brace = self.stack.pop()
-                    if (char != matching_braces.get(popped_opening_brace)) :
-                        return char + " 일치하지 않는 유형의 여는 괄호가 있음"
-        if (self.stack.read()) :
-            return self.stack.read() + " 닫는 괄호가 없음"
-        return True
-X = Linter()
-text = input()
-print(X.lint(text))
+        self.data = []
+    def push (self, text) :
+        self.data.append(text)
+    def pop (self) :
+        if (len(self.data) == 0) :
+            return None
+        return self.data.pop()
+    def read (self) :
+        if (len(self.data) == 0) :
+            return None
+        return self.data[-1]
+priority = {'+' : 1, '-' : 1, '*' : 2, '/' : 2}
+expression = input()
+operator = stack()
+for value in expression :
+    if (value in string.ascii_uppercase) :
+        print(value, end="")
+    elif (value == '(') :
+        operator.push(value)
+    elif (value == ')') :
+        while (operator.read() != '(') :
+            print(operator.pop(), end="")
+        operator.pop()
+    else :
+        while (operator.read() is not None and operator.read() != '(' and priority[operator.read()] >= priority[value]) :
+            print(operator.pop(), end = "")
+        operator.push(value)
+while(operator.read() is not None) :
+    print(operator.pop(), end = "")
+
