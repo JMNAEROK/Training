c958cc146cdad773c0e364a83136f7614826d7bd
JM <kjm8024@gmail.com>
2026-01-20 17:06:40 +0900

    fibonacci with recursion

--- test.py
+++ test.py
@@ -1,22 +1,7 @@
-def compression (x, y, n) :
-    first = array[x][y]
-    same = True
-    for i in range(x, x + n) :
-        for j in range(y, y + n) :
-            if first != array[i][j] :
-                same = False
-                break
-    if same :
-        return first
-    half = n // 2
-    return "(" + \
-        compression(x, y, half) + \
-        compression(x, y+half, half) + \
-        compression(x+half, y, half) + \
-        compression(x+half, y+half, half) + \
-         ")"
-
+def fibonacci (N) :
+    if hash.get(N) is None :
+        hash[N] = fibonacci(N-1) + fibonacci(N-2)
+    return hash[N]
 N = int(input())
-array = list(input().strip() for i in range(N))
-print(array)
-print(compression(0, 0, N))
+hash = {0 : 0, 1 : 1}
+print(fibonacci(N))
