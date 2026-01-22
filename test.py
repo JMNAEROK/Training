4a63187c23aaac12f403afba51e69a92077f7945
JM <kjm8024@gmail.com>
2026-01-22 14:29:21 +0900

    triangular nubmer with recursion

--- test.py
+++ test.py
@@ -1,10 +1,6 @@
-def select_even (arr) :
-    if not arr :
-        return []
-    if arr[0] % 2 == 0 :
-        return [arr[0]] + select_even(arr[1:])
-    else :
-        return select_even(arr[1:])
-
-array = list(map(int, input().split()))
-print(select_even (array))
+def triangular_number (N) :
+    if N == 1 :
+        return 1
+    return triangular_number(N - 1) + N
+N = int(input())
+print(triangular_number(N))
