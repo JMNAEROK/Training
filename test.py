c8569bfc0e1c7b0251ce394642555e495ce5cf6b
JM <kjm8024@gmail.com>
2026-02-04 12:17:05 +0900

    DoublyLinkedList and Queue with DoublyLinkedList

--- test.py
+++ test.py
@@ -2,82 +2,43 @@ class Node :
     def __init__ (self, data) :
         self.data = data
         self.next_node = None
+        self.previous_node = None
 
-class LinkedList :
-    def __init__ (self, first_node = None) :
+class DoublyLinkedList :
+    def __init__ (self, first_node = None, last_node = None) :
         self.first_node = first_node
-    
-    def read (self, index) :
-        current_node = self.first_node
-        current_index = 0
-
-        while current_index < index :
-            current_node = current_node.next_node
-            current_index += 1
-
-            if not current_node :
-                return None
+        self.last_node = last_node
         
-        return current_node.data
-    
-    def search (self, value) :
-        current_node = self.first_node
-        current_index = 0
-
-        while True :
-            if current_node.data == value :
-                return current_index
-            
-            current_node = current_node.next_node
-
-            if not current_node :
-                break
-
-            current_index += 1
-        
-        return None
-    
-    def insert (self, index, value) :
+    def append (self, value) :
         new_node = Node(value)
 
-        if index == 0 :
-            new_node.next_node = self.first_node
+        if not self.first_node :
             self.first_node = new_node
-        
-        current_node = self.first_node
-        current_index = 0
+            self.last_node = new_node
+        else :
+            new_node.previous_node = self.last_node
+            self.last_node.next_node = new_node
+            self.last_node = new_node
+    
+    def pop_head (self) :
+        popped_node = self.first_node
+        self.first_node = self.first_node.next_node
+        self.first_node.previous_node = None
+        return popped_node
+    
+class Queue :
+    def __init__ (self) :
+        self.data = DoublyLinkedList()
+    
+    def enqueue (self, element) :
+        self.data.append(element)
 
-        while current_index < (index - 1) :
-            current_node = current_node.next_node
-            current_index += 1
-        
-        new_node.next_node = current_node.next_node
-        current_node.next_node = new_node
+    def dequeue (self) :
+        dequeued_node = self.data.pop_head()
+        return dequeued_node.data
+    
+    def read (self) :
+        if not self.data.first_node :
+            return None
+        return self.data.first_node.data
 
-    def delete (self, index) :
-        if index == 0 :
-            self.first_node = self.first_node.next_node
-            return
-        
-        current_node = self.first_node
-        current_index = 0
-
-        while current_index < (index - 1) :
-            current_node = current_node.next_node
-            current_index += 1
-        
-        node_after_deleted_node = current_node.next_node.next_node
-
-        current_node.next_node = node_after_deleted_node
-
-node_1 = Node("once")
-node_2 = Node("upon")
-node_3 = Node("a")
-node_4 = Node("time")
-node_1.next_node = node_2
-node_2.next_node = node_3
-node_3.next_node = node_4
-
-list = LinkedList(node_1)
-list.delete (1)
-print(list.read(1))
