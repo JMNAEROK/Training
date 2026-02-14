5a026a1a1be136f407b1d32ef998d5558f73945f
JM <kjm8024@gmail.com>
2026-02-14 11:08:50 +0900

    Binary Search Tree

--- test.py
+++ test.py
@@ -1,55 +1,97 @@
-class Node :
-    def __init__ (self, data) :
-        self.index = None
-        self.data = data
-        self.previous_node = None
-        self.next_node = None
+class TreeNode :
+    def __init__ (self, value, left=None, right=None) :
+        self.value = value
+        self.left_child = left
+        self.right_child = right
 
-class DoublelyLinkedList :
-    def __init__ (self) :
-        first_node = None
-        last_node = None
+node1 = TreeNode(25)
+node2 = TreeNode(75)
+root = TreeNode(50, node1, node2)
+
+def search (search_value, node) :
+    if not node or node.value == search_value :
+        return node
     
-N = int(input())
-int_array = list(map(int, input().split()))
-node_array = []
-for index, value in enumerate(int_array) :
-    node_array.append(Node(value))
-    node_array[index].index = index + 1
-
-#노드 연결
-for index in range(len(node_array)) :
-    if index == len(node_array) - 1 :
-        node_array[index].previous_node = node_array[index - 1]
-        node_array[index].next_node = node_array[0]
+    elif search_value < node.value :
+        return search(search_value, node.left_child)
+    
     else :
-        node_array[index].previous_node = node_array[index - 1]
-        node_array[index].next_node = node_array[index + 1]
-first_node = node_array[0]
-last_node = node_array[-1]
+        return search(search_value, node.right_child)
+    
+def insert (value, node) :
+    if value < node.value :
 
-ans = []
-current_node = node_array[0]
-for i in range(N) :
-    #인덱스 추가
-    ans.append(str(current_node.index))
-    move= current_node.data
-    #삭제
-    temp_prev_node = current_node.previous_node
-    temp_next_node = current_node.next_node
-    temp_prev_node.next_node = temp_next_node
-    temp_next_node.previous_node = temp_prev_node
-    
-    #
-    
-    if move > 0 :
-        current_node = temp_next_node
-        for j in range(move - 1) :
-            current_node = current_node.next_node
-    elif move < 0 :
-        current_node = temp_prev_node
-        for j in range(abs(move) - 1) :
-            current_node = current_node.previous_node
-    
+        if not node.left_child :
+            node.left_child = TreeNode(value)
+        else :
+            insert (value, node.left_child)
+        
+    elif value > node.value :
+        if not node.right_child :
+            node.right_child = TreeNode(value)
+        else :
+            insert(value, node.right_child)
 
-print(" ".join(ans))
+
+def replace_with_successor_node (node) :
+    successor_node = node.right_child
+
+    if not successor_node.left_child :
+        node.value = successor_node.value
+        node.right_child = successor_node.right_child
+        return
+    
+    while successor_node.left_child :
+        parent_of_successor_node = successor_node
+        successor_node = successor_node.left_child
+    
+    if successor_node.right_child :
+        parent_of_successor_node.left_child = successor_node.right_child
+    else :
+        parent_of_successor_node.left_child = None
+
+    node.value = successor_node.value
+    return successor_node
+
+def delete (value_to_delete, node) :
+    current_node = node
+    parent_of_current_node = None
+    node_to_delete = None
+
+    while current_node :
+        if current_node.value == value_to_delete :
+            node_to_delete = current_node
+            break
+        
+        parent_of_current_node = current_node
+        if value_to_delete < current_node.value :
+            current_node = current_node.left_child
+        elif value_to_delete > current_node.value :
+            current_node = current_node.right_child
+
+    if not node_to_delete :
+        return None
+    
+    if node_to_delete.left_child and node_to_delete.right_child :
+        replace_with_successor_node (node_to_delete)
+    else : #삭제된 노드에 자식이 0 또는 1개 있는 경우
+
+        child_of_deleted_node = (node_to_delete.left_child or node_to_delete.right_child)
+
+        if not parent_of_current_node :
+            node_to_delete.value = child_of_deleted_node.value
+            node_to_delete.left_child = child_of_deleted_node.left_child
+            node_to_delete.right_child = child_of_deleted_node.right_child
+        elif node_to_delete == parent_of_current_node.left_child :
+            parent_of_current_node.left_child = child_of_deleted_node
+        elif node_to_delete == parent_of_current_node.right_child :
+            parent_of_current_node.right_child = child_of_deleted_node
+
+    return node_to_delete
+
+def traverse_and_print (node) :
+    if not node :
+        return
+    traverse_and_print(node.left_child)
+    print(node.value)
+    traverse_and_print(node.right_child)
